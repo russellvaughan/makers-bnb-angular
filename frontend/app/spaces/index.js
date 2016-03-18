d7a947cd8014fe60e775cb1f39201abb990217d9
russellvaughan <russellvaughan@gmail.com>
2016-03-18 09:32:40 +0000

    individual page links added but links not working

--- frontend/app/spaces/index.js
+++ frontend/app/spaces/index.js
@@ -7,11 +7,11 @@ angular.module('makersbnb.index', ['ngRoute'])
   $routeProvider.when('/spaces/index', {
     templateUrl: 'spaces/index.html',
     controller: 'SpacesCtrl'
-  });
-  	// when('/spaces/:itemID', {
-  	// 	templateUrl: 'spaces/details.html',
-  	// 	controler: 'DetailsCtrl'
-  	// });
+  }).
+  	when('/spaces/:itemID', {
+  		templateUrl: 'spaces/details.html',
+  		controler: 'DetailsCtrl'
+  	});
 }])
 
 .controller('SpacesCtrl', ['$scope','$http', function($scope, $http) {
@@ -33,13 +33,13 @@ angular.module('makersbnb.index', ['ngRoute'])
 
 	$scope.spaces = $scope.getSpaces()
 
-}]);
+}])
 
 
-// .controller('DetailsCtrl', ['$scope','$http', '$routeParams', function($scope, $http, $routeParams) {
-// 	// $scope.getSpaces = function(){
-// 	// 	$http.get('http://localhost:3000/spaces.json').then(function(response){
-// 	// 		$scope.spaces = response.data;
-// 	// 		$scope.whichItem = $routeParams.itemID;
-// 	// 	})};
-// }])
+.controller('DetailsCtrl', ['$scope','$http', '$routeParams', function($scope, $http, $routeParams) {
+	$scope.getSpaces = function(){
+		$http.get('http://localhost:3000/spaces.json').then(function(response){
+			$scope.spaces = response.data;
+			$scope.whichItem = $routeParams.itemID;
+		})};
+}])
