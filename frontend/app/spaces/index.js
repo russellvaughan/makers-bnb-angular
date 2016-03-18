ed65fb3bca92094a64f87d3780b7dfbf262e2aae
russellvaughan <russellvaughan@gmail.com>
2016-03-18 09:05:30 +0000

    can add spaces the the database from angular

--- frontend/app/spaces/index.js
+++ frontend/app/spaces/index.js
@@ -8,11 +8,38 @@ angular.module('makersbnb.index', ['ngRoute'])
     templateUrl: 'spaces/index.html',
     controller: 'SpacesCtrl'
   });
+  	// when('/spaces/:itemID', {
+  	// 	templateUrl: 'spaces/details.html',
+  	// 	controler: 'DetailsCtrl'
+  	// });
 }])
-.controller('SpacesCtrl', ['$scope',function($scope) {
-	this.getSpaces = function(){
-		return [{id:1, name: "a nice place"}, {id: 2, name:"a horrible place"}];
+
+.controller('SpacesCtrl', ['$scope','$http', function($scope, $http) {
+	$scope.getSpaces = function(){
+		$http.get('http://localhost:3000/spaces.json').then(function(response){
+			$scope.spaces = response.data
+	})};
+
+	$scope.space = {}
+
+	$scope.createSpace = function(){
+	$http.post('http://localhost:3000/spaces', $scope.space).
+		success(function(data) {
+			console.log(":)")
+			$scope.getSpaces()
+		}).error(function(data) {
+		});
 	};
-	$scope.spaces = this.getSpaces()
+
+	$scope.spaces = $scope.getSpaces()
+
 }]);
 
+
+// .controller('DetailsCtrl', ['$scope','$http', '$routeParams', function($scope, $http, $routeParams) {
+// 	// $scope.getSpaces = function(){
+// 	// 	$http.get('http://localhost:3000/spaces.json').then(function(response){
+// 	// 		$scope.spaces = response.data;
+// 	// 		$scope.whichItem = $routeParams.itemID;
+// 	// 	})};
+// }])
